d9739eed5b9d20e67e581f9dfbc02ed0a478c208
Yura <yurpro@gmail.com>
2017-08-01 19:38:25 +0300

    Hello i'm chakt

---
+++ Lab2017.07.27/src/Hello.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/*
+public class Hello {
+
+    public static void main(String args[]) {
+        System.out.print("Hello!");//Вывисти на консль.
+    }
+
+}*/
+
+/*public class Hello{
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);//сканировать ввод с клавиатуры System.in
+        String line = sc.nextLine();
+        System.out.println("Hello " + line);
+    }
+
+}*/
+class Caunt{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(sc.nextLine());
+
+    }
+
+
+}
